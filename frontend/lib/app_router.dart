dffe5da8e6ee4b0a18926eb6dcd4f8df5fa6e072
Shehab Diab <shehabgehaddiab@gmail.com>
2025-05-07 02:57:29 +0300

    wrap RestaurantsScreen with BlocProvider

--- frontend/lib/app_router.dart
+++ frontend/lib/app_router.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/constants/strings.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:frontend/business_logic/cubit/restaurants_cubit.dart';
+// import 'package:frontend/constants/strings.dart';
+import 'package:frontend/data/repository/restaurants_repository.dart';
+import 'package:frontend/data/web_services/restaurants_web_services.dart';
 import 'package:frontend/screens/register_screen.dart';
 import 'package:frontend/screens/restaurant_details_screen.dart';
 import 'package:frontend/screens/restaurants_screen.dart';
@@ -9,9 +13,20 @@ class AppRouter {
   Route? generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case '/restaurants':
-        return MaterialPageRoute(builder: (_) => const RestaurantScreen());
+        return MaterialPageRoute(
+          builder:
+              (_) => BlocProvider(
+                create:
+                    (context) => RestaurantsCubit(
+                      restaurantsRepository: RestaurantsRepository(
+                        restaurantsWebServices: RestaurantsWebServices(),
+                      ),
+                    ),
+                child: const RestaurantsScreen(),
+              ),
+        );
 
-      case restaurantDetailScreen:
+      case '/restaurant_detail':
         return MaterialPageRoute(
           builder: (_) => const RestaurantDetailsScreen(),
         );
