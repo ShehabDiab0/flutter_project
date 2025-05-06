652f94e3c93b2279a1ce96e855d62f808a93a40f
Shehab Diab <shehabgehaddiab@gmail.com>
2025-05-06 23:01:41 +0300

    add AppRouter for navigation

---
+++ frontend/lib/app_router.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/constants/strings.dart';
+import 'package:frontend/presentation/screens/login_screen.dart';
+import 'package:frontend/presentation/screens/register_screen.dart';
+import 'package:frontend/presentation/screens/restaurant_details_screen.dart';
+import 'package:frontend/presentation/screens/restaurants_screen.dart';
+
+class AppRouter {
+  Route? generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case restaurantsScreen:
+        return MaterialPageRoute(builder: (_) => const RestaurantScreen());
+
+      case restaurantDetailScreen:
+        return MaterialPageRoute(
+          builder: (_) => const RestaurantDetailsScreen(),
+        );
+
+      case 'login':
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+
+      case 'register':
+        return MaterialPageRoute(builder: (_) => const RegisterScreen());
+
+      default:
+        return MaterialPageRoute(
+          builder:
+              (_) =>
+                  const Scaffold(body: Center(child: Text("Page Not Found"))),
+        );
+    }
+  }
+}
