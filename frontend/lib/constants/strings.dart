bc7f3d98a07863f02eac932a74ffd3d07c129e51
Shehab Diab <shehabgehaddiab@gmail.com>
2025-05-06 23:45:37 +0300

    remove / from register and login URLs

--- frontend/lib/constants/strings.dart
+++ frontend/lib/constants/strings.dart
@@ -1,6 +1,6 @@
 final baseURL = 'http://10.0.2.2:8000/';
-final registerURL = '/accounts/register/';
-final loginURL = '/accounts/login/';
+final registerURL = 'accounts/register/';
+final loginURL = 'accounts/login/';
 
 // Screens for app_router.dart
 const restaurantsScreen = '/';
