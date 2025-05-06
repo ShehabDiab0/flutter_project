ea5bea06965abb7c3418770b5a783352487396ff
Shehab Diab <shehabgehaddiab@gmail.com>
2025-05-06 23:00:35 +0300

    add auth urls and screen names

---
+++ frontend/lib/constants/strings.dart
@@ -0,0 +1,10 @@
+final baseURL = 'http://10.0.2.2:8000/';
+final registerURL = '/accounts/register/';
+final loginURL = '/accounts/login/';
+
+// Screens for app_router.dart
+const restaurantsScreen = '/';
+const restaurantDetailScreen = '/restaurant_details';
+const favoriteRestaurantsScreen = '/favorite_restaurants';
+const searchRestaurantsScreen = '/search_restaurants';
+const productDetailsScreen = '/product_details';
