5da6481ea77a69c94eef49e5082abe9e72e1170c
Shehab Diab <shehabgehaddiab@gmail.com>
2025-05-06 23:49:16 +0300

    remove extra fields in user model

--- frontend/lib/data/models/user.dart
+++ frontend/lib/data/models/user.dart
@@ -1,37 +1,25 @@
-class CustomUser {
+class User {
   final String name;
   final String email;
   final String? gender;
   final int? level;
   final String? profilePicture;
-  final bool isActive;
-  final bool isStaff;
-  final List<String>? groups;
-  final List<String>? userPermissions;
 
-  CustomUser({
+  User({
     required this.name,
     required this.email,
     this.gender,
     this.level,
     this.profilePicture,
-    required this.isActive,
-    required this.isStaff,
-    this.groups,
-    this.userPermissions,
   });
 
-  factory CustomUser.fromJson(Map<String, dynamic> json) {
-    return CustomUser(
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
       name: json['name'],
       email: json['email'],
       gender: json['gender'],
       level: json['level'],
       profilePicture: json['profile_picture'],
-      isActive: json['is_active'],
-      isStaff: json['is_staff'],
-      groups: List<String>.from(json['groups'] ?? []),
-      userPermissions: List<String>.from(json['user_permissions'] ?? []),
     );
   }
 
@@ -42,10 +30,6 @@ class CustomUser {
       'gender': gender,
       'level': level,
       'profile_picture': profilePicture,
-      'is_active': isActive,
-      'is_staff': isStaff,
-      'groups': groups,
-      'user_permissions': userPermissions,
     };
   }
 }
