100b0e3bdd41d72036b32d700cadad5ca362105d
Shehab Diab <shehabgehaddiab@gmail.com>
2025-05-07 04:42:45 +0300

    add Restaurant object to Product model

--- frontend/lib/data/models/product.dart
+++ frontend/lib/data/models/product.dart
@@ -1,47 +1,36 @@
+import 'package:frontend/data/models/restaurant.dart';
+
 class Product {
   final int id;
-  final int restaurantId;
+  final Restaurant restaurant;
   final String name;
   final String description;
   final double price;
   final String? imageUrl;
-  final String? category;
+  final String category;
   final DateTime createdAt;
 
   Product({
     required this.id,
-    required this.restaurantId,
+    required this.restaurant,
     required this.name,
     required this.description,
     required this.price,
     this.imageUrl,
-    this.category,
+    required this.category,
     required this.createdAt,
   });
 
   factory Product.fromJson(Map<String, dynamic> json) {
     return Product(
       id: json['id'],
-      restaurantId: json['restaurant'], // Typically the foreign key value
+      restaurant: Restaurant.fromJson(json['restaurant']),
       name: json['name'],
       description: json['description'],
-      price: (json['price'] as num).toDouble(),
+      price: double.tryParse(json['price']) ?? 0.0,
       imageUrl: json['image_url'],
-      category: json['category'],
+      category: json['category'] ?? 'Uncategorized',
       createdAt: DateTime.parse(json['created_at']),
     );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'restaurant': restaurantId,
-      'name': name,
-      'description': description,
-      'price': price,
-      'image_url': imageUrl,
-      'category': category,
-      'created_at': createdAt.toIso8601String(),
-    };
   }
 }
